c0486921ac401f7eb2fea4f588ff7250bb3ef6bf
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2023-01-17 02:36:30 +0100

    dog animation start

---
+++ app.js
@@ -0,0 +1,64 @@
+let playerState = "sit";
+const dropDown = document.getElementById('animations');
+dropDown.addEventListener('change', function (e) {
+    playerState = e.target.value;
+})
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+const playerImage = new Image();
+playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteheight = 523;
+
+
+let gameFrame = 0;
+const staggerFrame = 5;
+const spriteAnimation = [];
+const animationState = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 9,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    },
+];
